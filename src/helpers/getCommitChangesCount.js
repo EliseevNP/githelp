3224dd9f40c28b70a38ff0d55ee23be13a2bf483
Nikita Eliseev <npeliseev@gmail.com>
2019-10-11 11:33:15 +0300

    fix commit command

--- src/helpers/getCommitChangesCount.js
+++ src/helpers/getCommitChangesCount.js
@@ -2,4 +2,4 @@ const exec = require('./exec');
 
 // Returns the number of files added to the commit
 // If folder located in 'repository' path is not git repository, then function return 0
-module.exports = async repository => +(await exec(`cd ${repository} && git status --porcelain | grep "^A" | wc -l`)).stdout;
+module.exports = async repository => +(await exec(`cd ${repository} && git status --porcelain | grep "^M" | wc -l`)).stdout;
